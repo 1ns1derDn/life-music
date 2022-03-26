55e557d4b8f687f0c13ad8ad27d94921fe68c87c
1ns1derDn <sergienko-98@mail.ua>
2022-03-26 17:17:39 +0200

    added setting project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      loader: "@svgr/webpack",
+      issuer: /\.[jt]sx$/,
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              removeViewBox: false,
+            },
+          ],
+        },
+        titleProp: true,
+      },
+      test: /\.svg$/,
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  experimental: {
+    outputStandalone: true,
+  },
+};
+
+module.exports = nextConfig;
